064106fcc3373a880350ccff95ae9dc7645d7255
Chris Norman <cnorman@broadinstitute.org>
2018-10-24 17:48:38 -0400

    Update read/reference input arguments to use GATKPathSpecifier and start removing references to javai.io.File.

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/OptionalReferenceInputArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/OptionalReferenceInputArgumentCollection.java
@@ -2,8 +2,11 @@ package org.broadinstitute.hellbender.cmdline.argumentcollections;
 
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.hellbender.cmdline.StandardArgumentDefinitions;
+import org.broadinstitute.hellbender.engine.GATKInputPath;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * An argument collection for use with tools that optionally accept a reference file as input.
@@ -12,10 +15,18 @@ public final class OptionalReferenceInputArgumentCollection extends ReferenceInp
     private static final long serialVersionUID = 1L;
 
     @Argument(fullName = StandardArgumentDefinitions.REFERENCE_LONG_NAME, shortName = StandardArgumentDefinitions.REFERENCE_SHORT_NAME, doc = "Reference sequence", optional = true)
-    private String referenceFileName;
+    private GATKInputPath referenceInput;
 
     @Override
     public String getReferenceFileName() {
-        return referenceFileName;
+        if (referenceInput != null) {
+            final String uriString = referenceInput.getURIString();
+            final File tempFile = new File(referenceInput.getURIString());
+            final boolean fExist = tempFile.exists();
+            Path pPath = Paths.get(referenceInput.getURI());
+            //final boolean pExists = pPath.g
+            int i = 37;
+        }
+        return referenceInput != null ? referenceInput.getURIString() : null;
     }
 }
